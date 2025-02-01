a70c8504302fb332ea1f939538f3e45b463ffb9f
Ben <benkuchma@gmail.com>
2025-02-01 19:51:43 +0000

    pushed current broken scoring file

--- answers.py
+++ answers.py
@@ -1,14 +1,14 @@
 from bloqade import move
 from helper_functions import *
 
-def main():
+def answer_1_1():
     q = move.NewQubitRegister(3)
 
     state = move.Init(qubits=[q[0],q[1],q[2]], indices=[0,1,2])
     state.gate[[0,1,3]] = move.Move(state.storage[[0,1,2]])
     state = move.GlobalCZ(atom_state=state)
-    state.gate[[2]] = move.Move(state.gate[[1])
-    state. = local_H(state=state,target_indices=[2])
+    state.gate[[2]] = move.Move(state.gate[[1]])
+    state = local_H(state=state,target_indices=[2])
     state.storage[[0,1,2]] = move.Move(state.gate[[0,2,3]])
-    move.Execute(state)
     return state
+    # move.Execute(state)
