1cfee828df3dc7bcda5d7badc775fb851f4acf14
Ben <benkuchma@gmail.com>
2025-02-01 20:45:11 +0000

    updated helper functions

--- answers.py
+++ answers.py
@@ -9,10 +9,9 @@ def answer_1a():
     state.gate[[0,1,3]] = move.Move(state.storage[[0,1,2]])
     state = move.GlobalCZ(atom_state=state)
     state.gate[[2]] = move.Move(state.gate[[1]])
-    state = local_H(state=state,target_indices=[2])
+    state = local_H(atom_state=state,target_indices=[2])
     state.storage[[0,1,2]] = move.Move(state.gate[[0,2,3]])
     return state
-    # move.Execute(state)
 
 @move.vmove
 def answer_1b():
