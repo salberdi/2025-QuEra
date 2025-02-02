11fdb11561e1f9000d5fb909c100cb06af591d49
odepoint <owen.depoint@gmail.com>
2025-02-02 03:49:59 +0000

    undid last two commits

--- test.py
+++ test.py
@@ -16,6 +16,7 @@ for passive in range(10):
     aggressive.Fold(move.vmove)(main)
 
 expected_qasm = """
+
 OPENQASM 2.0;
 include "qelib1.inc";
 
@@ -24,7 +25,9 @@ include "qelib1.inc";
 qreg q[3];
 
 
-ccx q[0],q[1],q[2];
+cz q[0],q[1];
+cx q[2],q[1];
+
 """
 
 scorer = MoveScorer(main, expected_qasm)
