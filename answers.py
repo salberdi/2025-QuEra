1cc423b9c8e6dda529a6be0d8b21ec587167432e
Sebastian Alberdi <salberdi@mit.edu>
2025-02-02 05:02:04 +0000

    stuff

--- answers.py
+++ answers.py
@@ -54,7 +54,29 @@ def answer_1b():
 
 @move.vmove()
 def answer_2():
-    return 0
+    q = move.NewQubitRegister(3)
+
+    state = move.Init(qubits=[q[0],q[1],q[2]], indices=[0,1,2])
+    state.gate[[2,1,0]] = move.Move(state.storage[[0,1,2]])
+    state = move.LocalRz(atom_state=state, phi = pi * .125, indices=[1])
+    state = local_RX(atom_state=state, angle = pi * .125, indices=[0])
+    state = move.GlobalCZ(atom_state=state)
+    state = local_RX(atom_state=state, angle = -pi *.125, indices=[0])
+    state = move.GlobalCZ(atome_state=state)
+    state = local_RX(atom_state=state, angle = pi *.25, indices=[0])
+    state = move.LocalRz(atom_state=state, phi = pi * .25, indices = [2])
+    state.gate[[3]] = move.Move(state.gate[[0]])
+    state = move.GlobalCZ(atom_state=state)
+    state = local_RX(atom_state=state, angle = -pi *.25, indices=[3])
+    state = move.GlobalCZ(atom_state=state)
+    state.gate[[4,5]] = move.Move(state.gate[[0,2]])
+    state = local_RX(atom_state=state, angle = pi *.25, indices=[4])
+    state = move.LocalRz(atom_state=state, phi = pi * .25, indices = [5])
+    state = move.GlobalCZ(atom_state=state)
+    state = local_RX(atom_state=state, angle = -pi *.25, indices=[4])
+    state = move.GlobalCZ(atom_state=state)
+    state = global_H(atom_state=state)
+    return state
 
 @move.vmove()
 def answer_3():
