8f838a6606b2982563f5cb8418e6fc6b9abfc114
odepoint <owen.depoint@gmail.com>
2025-02-02 03:11:47 +0000

    add dag T, toffoli inefficient attempt

--- answers.py
+++ answers.py
@@ -16,12 +16,41 @@ def answer_1a():
 def answer_1b():
     q = move.NewQubitRegister(3)
 
-    state = move.Init(qubits=[q[0],q[1],q[2]], indices=[0,1,2])
-    state.gate[[0,1,3]] = move.Move(state.storage[[0,1,2]])
+
+    state = move.Init(qubits=[q[0], q[1], q[2]], indices=[0, 1, 2])
+    
+
+    def one_to_two():
+        state.gate[[0,1]] = move.Move(state.storage[[1,2]]) # 0 is 1, 1 is 2 
+        state = move.GlobalCZ(atom_state=state)
+        state = local_HTH(atom_state=state, indices=[1], dag=True)
+        state.storage[[1, 2]] = move.Move(state.gate[[0, 1]])
+
+
+    def zero_to_two():
+        state.gate[[0, 1]] = move.Move(state.storage[[0, 2]]) 
+        state = move.GlobalCZ(atom_state=state)
+        state = local_HTH(atom_state=state, indices=[1], dag=False)
+        state.storage[[0, 2]] = move.Move(state.gate[[0, 1]])
+
+    one_to_two()
+    zero_to_two()
+    one_to_two()
+    zero_to_two()
+
+    state.gate[[0,1]] = move.Move(state.storage[[0,1]])
+    state = local_T(atom_state=state, indices=[1], dag=True)
+
+    state = local_H(atom_state=state, indices=[1])
     state = move.GlobalCZ(atom_state=state)
-    state.gate[[2]] = move.Move(state.gate[[1]])
-    state = local_H(atom_state=state,indices=[2])
+    state = local_HTH(atom_state=state, indices=[1], dag=True)
+    state = move.GlobalCZ(atom_state=state)
+
+    state = move.local_S(atom_state=state, indices=[1])
+    state = move.local_T(atom_state=state, indices=[0])
+
     return state
+
 
 @move.vmove()
 def answer_2():
