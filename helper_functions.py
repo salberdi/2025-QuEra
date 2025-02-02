06be5bc3b3d95b58b5fd33d8bdb652c710b7eb34
odepoint <owen.depoint@gmail.com>
2025-02-02 03:44:20 +0000

    cast ints to float

--- helper_functions.py
+++ helper_functions.py
@@ -3,15 +3,15 @@ from numpy import pi
 
 @move.vmove()
 def global_H(atom_state:move.core.AtomState):
-    state = move.GlobalXY(atom_state=atom_state,x_exponent=-1/2,axis_phase_exponent=1.0)
+    state = move.GlobalXY(atom_state=atom_state,x_exponent=-1.0/2,axis_phase_exponent=1.0)
     state = move.GlobalXY(atom_state=state,x_exponent=1,axis_phase_exponent=0.0)
     return state
 
 @move.vmove()
 def local_H(atom_state:move.core.AtomState,indices):
-    state = move.GlobalXY(atom_state=atom_state,x_exponent=1/4,axis_phase_exponent=1/2)
-    state = move.LocalRz(atom_state=state,phi=1,indices=indices)
-    state = move.GlobalXY(atom_state=state,x_exponent=-1/4,axis_phase_exponent=1/2)
+    state = move.GlobalXY(atom_state=atom_state,x_exponent=1.0/4,axis_phase_exponent=1.0/2)
+    state = move.LocalRz(atom_state=state,phi=1.0,indices=indices)
+    state = move.GlobalXY(atom_state=state,x_exponent=-1.0/4,axis_phase_exponent=1.0/2)
     return state
 
 @move.vmove()
@@ -33,26 +33,26 @@ def local_RX(atom_state:move.core.AtomState,angle,indices):
 
 @move.vmove()
 def local_T(atom_state:move.core.AtomState,indices, dag=False):
-    state = move.LocalRz(atom_state=atom_state,phi=1/4 * (-1**dag),indices=indices)
+    state = move.LocalRz(atom_state=atom_state,phi=1.0/4 * (-1**dag),indices=indices)
     return state
 
 @move.vmove()
 def global_T(atom_state:move.core.AtomState):
-    state = move.GlobalRz(atom_state=atom_state,phi=1/4)
+    state = move.GlobalRz(atom_state=atom_state,phi=1.0/4)
     return state
 
 @move.vmove()
 def local_S(atom_state:move.core.AtomState,indices):
-    state = move.LocalRz(atom_state=atom_state,phi=1/2,indices=indices)
+    state = move.LocalRz(atom_state=atom_state,phi=1.0/2,indices=indices)
     return state
 
 @move.vmove()
 def global_S(atom_state:move.core.AtomState):
-    state = move.GlobalRz(atom_state=atom_state,phi=1/2)
+    state = move.GlobalRz(atom_state=atom_state,phi=1.0/2)
     return state
 
 @move.vmove()
 def local_HTH(atom_state:move.core.AtomState,indices, dag=False):
-    theta = 1/4 * (-1**dag)
+    theta = 1.0/4 * (-1.0**dag)
     state = local_RX(atom_state=atom_state,angle=theta,indices=indices)
     return state
